989b43c4a9ffbfbdc6bbb65fd70c9931b3bb3499
Adan98 <shahruramadan98@gmail.com>
2025-04-10 22:52:04 +0800

    Add authentication routes and views for login and registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,12 @@
-<h1>Hello Laravel</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>CookEase</title>
+</head>
+<body>
+    <h1>Welcome to CookEase!</h1>
+    <a href="/login">Login</a> |
+    <a href="/register">Register</a>
+</body>
+</html>
